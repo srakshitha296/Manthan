84c1a79c6f50e3d24a27d976fb662c7fab878f4f
darshan45672 <drshnbhandary@gmail.com>
2024-07-04 21:37:23 +0530

    updated icons of resources

--- app/Filament/Resources/ActiviyTypeResource.php
+++ app/Filament/Resources/ActiviyTypeResource.php
@@ -20,7 +20,7 @@ class ActiviyTypeResource extends Resource
 {
     protected static ?string $model = ActiviyType::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-group';
 
     public static function form(Form $form): Form
     {
