4a66320020c20b77915cee851cb3852c6edc0d3a
darshan45672 <drshnbhandary@gmail.com>
2024-07-04 21:35:09 +0530

    designed Student and Activity panel

--- app/Filament/Resources/StudentResource.php
+++ app/Filament/Resources/StudentResource.php
@@ -7,6 +7,8 @@ use App\Filament\Resources\StudentResource\RelationManagers;
 use App\Filament\Resources\StudentResource\RelationManagers\ActivitiesRelationManager;
 use App\Models\Student;
 use Filament\Forms;
+use Filament\Forms\Components\Group;
+use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
@@ -27,12 +29,37 @@ class StudentResource extends Resource
     {
         return $form
             ->schema([
-                Select::make('user_id')->required()->relationship('user', 'name'),
-                Select::make('college_id')->required()->relationship('college', 'name'),
-                Select::make('department_id')->required()->relationship('department', 'name'),
-                TextInput::make('usn')->required()->maxLength(255),
-                TextInput::make('semester')->required()->maxLength(255),
-            ]);
+                Group::make()->schema([
+                    Section::make('Student Name')->schema([
+                        Select::make('user_id')->relationship('user', 'name', fn($query) => $query->where('role', 'student'))
+                            ->searchable()->required(),
+                    ])
+                ])->columnSpan(1),
+                Group::make()->schema([
+                    Section::make('College Details')->schema([
+                        Select::make('college_id')->relationship('college', 'name')->searchable()->required(),
+                    ]),
+                ])->columnSpan(1),
+                Group::make()->schema([
+                    Section::make('Department Details')->schema([
+                        Select::make('department_id')->relationship('department', 'name')->searchable()->required(),
+                    ]),
+                ])->columnSpan(1),
+                Group::make()->schema([
+                    Section::make('Student Details')->schema([
+                        TextInput::make('usn')->required()
+                            ->rules(['regex:/^[0-9]{1}[A-Za-z]{2}[0-9]{2}[A-Za-z]{2}[0-9]{3}$/'])
+                            ->validationAttribute('USN')
+                            ->helperText('Ex: 4JK21CS016 or 4jk21cs016'),
+                        Select::make('semester')->required()->options([
+                            '1' => '1 st',
+                            '2' => '2 nd',
+                            '3' => '3 rd',
+                            '4' => '4 th',
+                        ]),
+                    ])->columns(2),
+                ])->columnSpan(1),
+            ])->columns(2);
     }
 
     public static function table(Table $table): Table
@@ -63,7 +90,7 @@ class StudentResource extends Resource
     public static function getRelations(): array
     {
         return [
-            ActivitiesRelationManager::class    
+            ActivitiesRelationManager::class
         ];
     }
 
