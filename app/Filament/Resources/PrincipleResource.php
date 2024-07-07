cf1ec365953f8009bf1474b0c8269c439d8dcf2d
darshan45672 <drshnbhandary@gmail.com>
2024-07-07 21:01:37 +0530

    created new resource programs

--- app/Filament/Resources/PrincipleResource.php
+++ app/Filament/Resources/PrincipleResource.php
@@ -27,7 +27,7 @@ class PrincipleResource extends Resource
 {
     protected static ?string $model = Principle::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-shield-exclamation';
 
     public static function form(Form $form): Form
     {
