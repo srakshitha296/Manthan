9b303e0e618a24d9fe58c28f668a2aedbf21ebed
darshan45672 <drshnbhandary@gmail.com>
2024-07-15 07:55:51 +0530

    created migration file for poe and modified image column of activity

---
+++ database/migrations/2024_07_15_020641_create_p_o_e_s_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('p_o_e_s', function (Blueprint $table) {
+            $table->id();
+            $table->string('label');
+            $table->string('name');
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('p_o_e_s');
+    }
+};
