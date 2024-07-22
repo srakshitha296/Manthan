08bcf18834f3220ca49a87e618989ebb258c894f
darshan45672 <drshnbhandary@gmail.com>
2024-07-22 19:33:11 +0530

    overrided notifications

--- app/Filament/Resources/UserResource/Pages/EditUser.php
+++ app/Filament/Resources/UserResource/Pages/EditUser.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\UserResource\Pages;
 
 use App\Filament\Resources\UserResource;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
 
 class EditUser extends EditRecord
@@ -15,5 +16,12 @@ class EditUser extends EditRecord
         return [
             Actions\DeleteAction::make(),
         ];
+    }
+
+    protected function getSavedNotification(): ?Notification {
+        return Notification::make()
+            ->success()
+            ->title('User Updated Successfully')
+            ->body('The user has been updated successfully.');
     }
 }
