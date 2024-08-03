f8181697c9fd0707c1cd5f11a72647bdeefedeee
darshan45672 <drshnbhandary@gmail.com>
2024-08-03 19:24:13 +0530

    fix: principle resource display

--- app/Filament/Resources/CollegeResource/RelationManagers/PrincipleRelationManager.php
+++ app/Filament/Resources/CollegeResource/RelationManagers/PrincipleRelationManager.php
@@ -3,9 +3,15 @@
 namespace App\Filament\Resources\CollegeResource\RelationManagers;
 
 use Filament\Forms;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Group;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -18,10 +24,38 @@ class PrincipleRelationManager extends RelationManager
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('user_id')
-                    ->required()
-                    ->maxLength(255),
-            ]);
+                Group::make()->schema([
+                    Section::make('Principle Name')->schema([
+                        Select::make('user_id')->relationship('user', 'name', fn($query) => $query->where('role', 'Principle'))
+                            ->searchable()->preload()->required(),
+                    ])
+                ])->columnSpan(1),
+                Group::make()->schema([
+                    Section::make('College Details')->schema([
+                        Select::make('college_id')->relationship('college', 'name')->preload()->searchable()->required(),
+                    ]),
+                ])->columnSpan(1),
+                Group::make()->schema([
+                    Section::make('College Details')->schema([
+                        Select::make('qualification')->options([
+                            'B.Tech' => 'B.Tech',
+                            'M.Tech' => 'M.Tech',
+                            'PhD' => 'PhD',
+                            'M.Sc' => 'M.Sc',
+                            'B.Sc' => 'B.Sc',
+                        ])->required()->multiple(),
+                        Select::make('specialization')->options([
+                            'B.Tech' => 'B.Tech',
+                            'M.Tech' => 'M.Tech',
+                            'PhD' => 'PhD',
+                            'M.Sc' => 'M.Sc',
+                            'B.Sc' => 'B.Sc',
+                        ])->required()->multiple(),
+                        TextInput::make('experience')->required()->maxLength(255),
+                        DatePicker::make('joining_date')->required(),
+                    ])->columns(2),
+                ])->columnSpan(2),
+            ])->columns(2);
     }
 
     public function table(Table $table): Table
@@ -29,7 +63,14 @@ class PrincipleRelationManager extends RelationManager
         return $table
             ->recordTitleAttribute('user_id')
             ->columns([
-                Tables\Columns\TextColumn::make('user_id'),
+                TextColumn::make('user.name')->numeric(),
+                TextColumn::make('college.name')->numeric(),
+                TextColumn::make('qualification')->searchable(),
+                TextColumn::make('experience')->searchable()->suffix(' years'),
+                TextColumn::make('specialization')->searchable(),
+                TextColumn::make('joining_date')->searchable(),
+                TextColumn::make('created_at')->dateTime()->sortable()->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')->dateTime()->sortable()->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
                 //
