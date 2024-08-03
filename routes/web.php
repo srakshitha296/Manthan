af6cf48dec0818ba7ac2f7c09035708dbda884d1
darshan45672 <drshnbhandary@gmail.com>
2024-08-03 12:16:04 +0530

    clean up

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-
-
 require __DIR__.'/auth.php';
