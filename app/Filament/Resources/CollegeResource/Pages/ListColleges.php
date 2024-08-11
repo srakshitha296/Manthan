275b5f8922e692522d55b4a9606c911e766a42d8
darshan45672 <drshnbhandary@gmail.com>
2024-08-11 19:08:16 +0530

    implemented: export feature for college extra addon detials has issues //commented out//

--- app/Filament/Resources/CollegeResource/Pages/ListColleges.php
+++ app/Filament/Resources/CollegeResource/Pages/ListColleges.php
@@ -2,9 +2,13 @@
 
 namespace App\Filament\Resources\CollegeResource\Pages;
 
+use App\Exports\CollegesExport;
 use App\Filament\Resources\CollegeResource;
+use App\Models\College;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Collection;
+use Maatwebsite\Excel\Facades\Excel;
 
 class ListColleges extends ListRecords
 {
@@ -12,8 +16,19 @@ class ListColleges extends ListRecords
 
     protected function getHeaderActions(): array
     {
-        return [
-            Actions\CreateAction::make(),
-        ];
+        if(College::count()){
+            return[
+                Actions\CreateAction::make(),
+                Actions\Action::make('exportCollege')->label('Export College')->icon('heroicon-o-document-arrow-down')
+                    ->action(function (Collection $records) {
+                        return Excel::download(new CollegesExport($records, 0), 'colleges.xlsx');
+                    })
+            ];
+        }else{
+            return[
+                Actions\CreateAction::make(),
+            ];
+        }
     }
+
 }
