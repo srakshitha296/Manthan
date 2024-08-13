5ef1cd71f2f7beb1b578487e530779a2f767cf5a
darshan45672 <drshnbhandary@gmail.com>
2024-08-13 20:08:49 +0530

    added PEOexport

--- app/Filament/Resources/ProgramExpectedOutcomesResource/Pages/ListProgramExpectedOutcomes.php
+++ app/Filament/Resources/ProgramExpectedOutcomesResource/Pages/ListProgramExpectedOutcomes.php
@@ -2,9 +2,13 @@
 
 namespace App\Filament\Resources\ProgramExpectedOutcomesResource\Pages;
 
+use App\Exports\ProgramExpectedOutcomesExport;
 use App\Filament\Resources\ProgramExpectedOutcomesResource;
+use App\Models\ProgramExpectedOutcomes;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Collection;
+use Maatwebsite\Excel\Facades\Excel;
 
 class ListProgramExpectedOutcomes extends ListRecords
 {
@@ -12,8 +16,18 @@ class ListProgramExpectedOutcomes extends ListRecords
 
     protected function getHeaderActions(): array
     {
-        return [
-            Actions\CreateAction::make(),
-        ];
+        if(ProgramExpectedOutcomes::count()){
+            return[
+                Actions\CreateAction::make(),
+                Actions\Action::make('exportPOE')->label('Export PEOs')->icon('heroicon-o-document-arrow-down')
+                    ->action(function (Collection $records) {
+                        return Excel::download(new ProgramExpectedOutcomesExport($records, 0), 'PEOs.xlsx');
+                    })
+            ];
+        }else{
+            return[
+                Actions\CreateAction::make(),
+            ];
+        }
     }
 }
