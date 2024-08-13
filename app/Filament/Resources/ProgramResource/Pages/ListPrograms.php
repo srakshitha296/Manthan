6d0382b7399e0e3cb74976fb54961b43d71cde07
darshan45672 <drshnbhandary@gmail.com>
2024-08-13 22:17:20 +0530

    implemented Program export module

--- app/Filament/Resources/ProgramResource/Pages/ListPrograms.php
+++ app/Filament/Resources/ProgramResource/Pages/ListPrograms.php
@@ -2,9 +2,13 @@
 
 namespace App\Filament\Resources\ProgramResource\Pages;
 
+use App\Exports\ProgramsExport;
 use App\Filament\Resources\ProgramResource;
+use App\Models\Program;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Collection;
+use Maatwebsite\Excel\Facades\Excel;
 
 class ListPrograms extends ListRecords
 {
@@ -12,8 +16,18 @@ class ListPrograms extends ListRecords
 
     protected function getHeaderActions(): array
     {
-        return [
-            Actions\CreateAction::make(),
-        ];
+        if(Program::count()){
+            return[
+                Actions\CreateAction::make(),
+                Actions\Action::make('exportProgram')->label('Export Program')->icon('heroicon-o-document-arrow-down')
+                    ->action(function (Collection $records) {
+                        return Excel::download(new ProgramsExport($records, 0), 'Programs.xlsx');
+                    })
+            ];
+        }else{
+            return[
+                Actions\CreateAction::make(),
+            ];
+        }
     }
 }
