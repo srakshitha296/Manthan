07717bddbdd7cf288592e73749b7acfe9a875af5
darshan45672 <drshnbhandary@gmail.com>
2024-08-16 19:07:24 +0530

    implement: user factory seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,8 +26,11 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'phone' => fake()->unique()->phoneNumber(),
+            'image' => fake()->image(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'address' => fake()->address(),
             'remember_token' => Str::random(10),
         ];
     }
