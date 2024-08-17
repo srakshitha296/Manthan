c191f4de9abe44f300ba5a5886437334507a8131
darshan45672 <drshnbhandary@gmail.com>
2024-08-17 19:21:09 +0530

    updated Registeration resource and its relationships

--- app/Filament/Resources/RegisteredEventResource/Pages/EditRegisteredEvent.php
+++ app/Filament/Resources/RegisteredEventResource/Pages/EditRegisteredEvent.php
@@ -3,7 +3,7 @@
 namespace App\Filament\Resources\RegisteredEventResource\Pages;
 
 use App\Filament\Resources\RegisteredEventResource;
-use Filament\Actions;
+use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
 
 class EditRegisteredEvent extends EditRecord
@@ -13,7 +13,7 @@ class EditRegisteredEvent extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            DeleteAction::make(),
         ];
     }
 }
