7d2cd391b10ee5e3b2279dc3db41f882c0891f5c
darshan45672 <drshnbhandary@gmail.com>
2024-08-21 19:44:37 +0530

    implement: board member

--- database/migrations/2024_08_17_103026_create_board_members_table.php
+++ database/migrations/2024_08_17_103026_create_board_members_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('board_members', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDeleteCascade();
+
             $table->timestamps();
         });
     }
