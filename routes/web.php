381426f8572e9abf89cd7038a30516710408f1c9
Rakshitha Shetty <111368698+srakshitha296@users.noreply.github.com>
2024-08-24 22:20:45 +0530

    Revert "Dashboard"

--- routes/web.php
+++ routes/web.php
@@ -8,14 +8,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', function () {
-    return view('home');
-});
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
