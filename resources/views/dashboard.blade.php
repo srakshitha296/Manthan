c661eb613e0d9f4d48d5cd01fbae6076ea0aeaf1
srakshitha296 <srakshitha296@gmail.com>
2024-08-24 20:54:29 +0530

    resolved conflict

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,7 @@
         <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
+            {{ __('dashboard') }}
         </h2>
     </x-slot>
 
