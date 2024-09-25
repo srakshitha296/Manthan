8774e9c197deb51a102b5f917c60bbf0153f8234
darshan45672 <drshnbhandary@gmail.com>
2024-09-26 00:25:35 +0530

    need to fix: column display

---
+++ app/Filament/Resources/BoardPresidentsResource/Pages/CreateBoardPresidents.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\BoardPresidentsResource\Pages;
+
+use App\Filament\Resources\BoardPresidentsResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateBoardPresidents extends CreateRecord
+{
+    protected static string $resource = BoardPresidentsResource::class;
+}
