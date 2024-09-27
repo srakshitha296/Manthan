efcee54caa4af76f488f9d57d2fe0505db495a28
darshan45672 <drshnbhandary@gmail.com>
2024-09-27 07:53:36 +0530

    fea:student notification

--- app/Filament/Resources/StudentResource/Pages/EditStudent.php
+++ app/Filament/Resources/StudentResource/Pages/EditStudent.php
@@ -3,12 +3,22 @@
 namespace App\Filament\Resources\StudentResource\Pages;
 
 use App\Filament\Resources\StudentResource;
+use App\Notifications\StudentNotification;
 use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
 
 class EditStudent extends EditRecord
 {
     protected static string $resource = StudentResource::class;
+
+    protected function afterSave(): void
+    {
+        // Get the student record after update
+        $student = $this->record;
+
+        // Notify the user associated with the student
+        $student->user->notify(new StudentNotification($student, 'updated'));
+    }
 
     protected function getHeaderActions(): array
     {
