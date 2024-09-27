93937677c6f1d38cdfa6ee385baec45db6501934
darshan45672 <drshnbhandary@gmail.com>
2024-09-27 08:03:59 +0530

    fea:faculty notification

--- app/Filament/Resources/FacultyResource/Pages/CreateFaculty.php
+++ app/Filament/Resources/FacultyResource/Pages/CreateFaculty.php
@@ -3,10 +3,26 @@
 namespace App\Filament\Resources\FacultyResource\Pages;
 
 use App\Filament\Resources\FacultyResource;
+use App\Notifications\FacultyNotification;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateFaculty extends CreateRecord
 {
     protected static string $resource = FacultyResource::class;
+
+    protected function afterCreate(): void
+    {
+        $faculty = $this->record;
+
+        // Dump the faculty to inspect the user_id
+        // dd($faculty->user_id, $faculty->user);
+
+        // Notify the user if found
+        if ($faculty->user) {
+            $faculty->user->notify(new FacultyNotification($faculty, 'created'));
+        } else {
+            dd('No associated user found for this student.');
+        }
+    }
 }
