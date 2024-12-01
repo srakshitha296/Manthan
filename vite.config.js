4ab1cbe14be3a77fb048890eb7b9874f35b4b859
srakshitha296 <srakshitha296@gmail.com>
2024-12-01 18:03:55 +0530

    modified vite.config

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,12 @@ export default defineConfig({
                 'resources/js/app.js',
             ],
             refresh: true,
+            valetTls: false, // Explicitly disable Valet TLS configuration
         }),
     ],
+    server: {
+        host: '127.0.0.1',
+        port: 5173,
+        strictPort: true, // Ensures port conflicts are caught early
+    },
 });
