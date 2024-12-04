b7b0d19d41434a9bdc7de31685c08da17e515111
darshan45672 <drshnbhandary@gmail.com>
2024-12-04 06:48:04 +0530

    implemented dashboard routes

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,11 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\GalleryController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -25,6 +22,8 @@ Route::get('/events-details/{id}',[EventController::class, 'show'])->name('event
 Route::post('/event-register/{id}',[EventController::class, 'register'])->name('event.register')->middleware('auth');
 
 Route::get('/gallery',[GalleryController::class, 'index'])->name('gallery');
+
+Route::get('/user-dashboard',[DashboardController::class,'index'])->name('user.dashboard')->middleware('auth');
 
 
 require __DIR__.'/auth.php';
