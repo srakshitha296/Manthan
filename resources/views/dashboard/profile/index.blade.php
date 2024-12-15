7c753199c391fab46b55d55a6957d241e324e3e0
darshan45672 <drshnbhandary@gmail.com>
2024-12-15 17:31:44 +0530

    added faculty module pages add and view all faculties

--- resources/views/dashboard/profile/index.blade.php
+++ resources/views/dashboard/profile/index.blade.php
@@ -168,7 +168,89 @@
 </div>  
 
 @elseif (Auth::user()->role == 'HoD')
-<h1>jingalalal</h1>
+<div class="mb-9">
+    <div class="row align-items-center justify-content-between g-3 mb-4">
+        <div class="col-auto">
+            <h2 class="mb-0">Profile</h2>
+        </div>
+        <div class="col-auto">
+            <div class="row g-3">
+                <div class="col-auto"><a href="{{ route('user.edit.profile') }}"><button class="btn btn-phoenix-primary" 
+                    ><span
+                            class="fa-solid fa-trash-can me-2"></span>Edit profile</button></a></div>
+                <div class="col-auto"><button class="btn btn-phoenix-secondary"><span
+                            class="fas fa-key me-2"></span>Reset password</button></div>
+            </div>
+        </div>
+    </div>
+    <div class="row g-5">
+        <div class="col-12 col-xxl-4">
+            <div class="row g-3 h-100">
+                <div class="col-12 col-md-7 col-xxl-12">
+                    <div class="card h-100 h-xxl-auto">
+                        <div class="card-body d-flex flex-column justify-content-between pb-3">
+                            <div class="row align-items-center g-5 mb-3 text-center text-sm-start">
+                                <div class="col-12 col-sm-auto mb-sm-2">
+                                    <div class="avatar avatar-5xl"><img class="rounded-circle"
+                                            src="../../../assets/img/team/15.webp" alt="" /></div>
+                                </div>
+                                <div class="col-12 col-sm-auto flex-1">
+                                    <h3>Ansolo Lazinatov</h3>
+                                    <p class="text-body-secondary">A J Institute of Engineering and
+                                        Technology,
+                                        Mangaluru </p>
+                                    <div><a class="me-2" href="#!"><span
+                                                class="fab fa-linkedin-in text-body-quaternary text-opacity-75 text-primary-hover"></span></a><a
+                                            class="me-2" href="#!"><span
+                                                class="fab fa-facebook text-body-quaternary text-opacity-75 text-primary-hover"></span></a><a
+                                            href="#!"><span
+                                                class="fab fa-twitter text-body-quaternary text-opacity-75 text-primary-hover"></span></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="d-flex flex-between-center border-top border-dashed pt-4">
+                                <div>
+                                    <h6>DESIGNATION</h6>
+                                    <p class="fs-9 text-body-secondary mb-0">Assistant Professor</p>
+                                </div>
+                                <div>
+                                    <h6>QUALIFICATION</h6>
+                                    <p class="fs-9 text-body-secondary mb-0">B.Tech, M.Tech, PhD</p>
+                                </div>
+                                <div>
+                                    <h6>SPECIALIZATION</h6>
+                                    <p class="fs-9 text-body-secondary mb-0">Cyber Security</p>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-5 col-xxl-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex align-items-center mb-3">
+                                <h3 class="me-1">Address</h3><button class="btn btn-link p-0"><span
+                                        class=" fs-8 ms-3 text-body-quaternary"></span></button>
+                            </div>
+                            <h5 class="text-body-secondary">Address</h5>
+                            <p class="text-body-secondary">Shatinon Mekalan<br />Vancouver, British
+                                Columbia<br />Canada
+                            </p>
+                            <div class="mb-3">
+                                <h5 class="text-body-secondary">Email</h5><a
+                                    href="mailto:shatinon@jeemail.com">shatinon@jeemail.com</a>
+                            </div>
+                            <h5 class="text-body-secondary">Phone</h5><a class="text-body-secondary"
+                                href="tel:+1234567890">+1234567890</a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>  
 
 @elseif (Auth::user()->role == 'Principle')
 <h1>Principle bvc</h1>
