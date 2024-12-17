bdfc4b9fd3b29dd9320af9378c56db8713ae3f23
darshan45672 <drshnbhandary@gmail.com>
2024-12-17 21:09:48 +0530

    fetch user testimonial

--- app/Http/Controllers/TestimonialController.php
+++ app/Http/Controllers/TestimonialController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Testimonials;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TestimonialController extends Controller
 {
     public function index(){
-        return view('dashboard.testimonial.index');
+        $user = Auth::user();
+        $testimonials = Testimonials::where('user_id', $user->id)->first();
+        // dd($testimonials);
+        return view('dashboard.testimonial.index', compact('testimonials'));
     }
 
     public function create(){
