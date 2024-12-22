fc6b6fb7d13e372421598000036f8ce5baf2087b
darshan45672 <drshnbhandary@gmail.com>
2024-12-22 14:05:39 +0530

    done add student by faculty

--- resources/views/dashboard/faculty/index.blade.php
+++ resources/views/dashboard/faculty/index.blade.php
@@ -7,7 +7,7 @@
             <!-- data-list='{"valueNames":["studentName","usn","start","end","points","projectprogress","status","action"],"page":6,"pagination":true}' -->
             <div class="row mb-4 gx-6 gy-3 align-items-center">
                 <div class="col-auto">
-                    <h2 class="mb-0">Faculties<span class="fw-normal text-body-tertiary ms-3">(30)</span>
+                    <h2 class="mb-0">Faculties<span class="fw-normal text-body-tertiary ms-3">({{ $faculties->count() }})</span>
                     </h2>
                 </div>
 
@@ -17,28 +17,14 @@
                 <table class="table fs-9 mb-0 border-top border-translucent">
                     <thead>
                         <tr>
-                            <th class="sort white-space-nowrap align-middle ps-0" scope="col"
-                                data-sort="faculty name" style="width:30%">NAME</th>
-                            <th class="sort align-middle ps-3" scope="col" data-sort="email"
-                                style="width:30%">EMAIL</th>
-                            <th class="sort align-middle ps-3" scope="col" data-sort="phone" style="width:30%;">
-                                PHONE
-                            </th>
-                            <th class="sort align-middle ps-3" scope="col" data-sort="designation"
-                                style="width:30%">
-                                DESIGNATION
-                            </th>
-                            <th class="sort align-middle ps-3" scope="col" data-sort="qualification"
-                                style="width:30%">
-                                QUALIFICATION
-                            </th>
-                            <th class="sort align-middle ps-3" scope="col" data-sort="experience"
-                                style="width:30%">
-                                EXPERIENCE
-                            </th>
-                            <th class="sort align-middle ps-3" scope="col" data-sort="doj" style="width:30%">
-                                D.O.J
-                            </th>
+                            <th class="sort white-space-nowrap align-middle ps-0" scope="col" data-sort="faculty name" style="width:10%">NAME</th>
+                            <th class="sort align-middle ps-3" scope="col" data-sort="email" style="width:10%">EMAIL</th>
+                            <th class="sort align-middle ps-3" scope="col" data-sort="phone" style="width:10%;"> PHONE</th>
+                            <th class="sort align-middle ps-3" scope="col" data-sort="designation" style="width:10%"> DESIGNATION </th>
+                            <th class="sort align-middle ps-3" scope="col" data-sort="qualification" style="width:10%"> QUALIFICATION </th>
+                            <th class="sort align-middle ps-3" scope="col" data-sort="experience" style="width:10%"> EXPERIENCE</th>
+                            <th class="sort align-middle ps-3" scope="col" data-sort="doj" style="width:10%"> D.O.J </th>
+                            <th class="sort align-middle ps-3" scope="col" data-sort="doj" style="width:10%"> Is Cordiantor </th>
                         </tr>
                     </thead>
                     <tbody class="list" id="project-list-table-body">
@@ -64,6 +50,9 @@
                             </td>
                             <td class="align-middle white-space-nowrap end ps-3 py-4">
                                 <p class="mb-0 fs-9 text-body">{{ \Carbon\Carbon::parse($faculty->joining_date)->format('M d, Y') }}</p>
+                            </td>
+                            <td class="align-middle white-space-nowrap end ps-3 py-4">
+                                <p class="mb-0 fs-9 text-body">{{ $faculty->is_cordinator ? "yes" : 'no' }}</p>
                             </td>
                         </tr>
                         @endforeach
